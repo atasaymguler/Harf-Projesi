33ce3d3234d17df76b6dd671bd8a58cdf85ce8e7
Atasay <130931984+atasaymguler@users.noreply.github.com>
2023-08-30 14:53:38 +0300

    Faker'ın Tüm Fonksiyonlarını Kullanma

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {lorem} from 'faker'; //Burada import ederiz.
- 
+import {faker} from '@faker-js/faker';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,5 +10,14 @@ import {lorem} from 'faker'; //Burada import ederiz.
 export class AppComponent {
   title = 'harf';
   randomText = lorem.sentence();  //Random gelen metinler randomText'e aktarılacaktır.
+
+  randomNumber =0;
+  
+
+  click()
+  {
+    this.randomNumber = faker.number.int();
+    console.log(this.randomNumber)
+  }
 
 }
